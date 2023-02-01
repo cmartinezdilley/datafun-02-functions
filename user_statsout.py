1260eca791f7e59fc906537bc06c68459b20c971
Christine Martinez <you@example.com>
2023-01-31 21:14:51 -0600

    Update

--- user_statsout.py
+++ user_statsout.py
@@ -4,7 +4,5 @@ The mean of the agility scores is 99.12.
 The median of the agility scores is 102.5.
 The variance of the agility scores is 179.25. 
 The standard deviation of the agility scores is 13.39. 
-Traceback (most recent call last):
-  File "/Users/nistine/Documents/datafun-02-functions-1/user_stats.py", line 97, in <module>
-    slope, intercept = statistics.linear_regression(x_times, y_temps)
-AttributeError: module 'statistics' has no attribute 'linear_regression'
+The best fit line has a slope of 2.78 and intercept of 3.12.
+The temp at hour 13 will be 39.0 degrees.
